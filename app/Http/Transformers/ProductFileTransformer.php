2e9fa8370b6a5627ac5a0d89c87dd2e9d52b4f14
Anhtd <anhtd@beetsoft.com.vn>
2022-12-06 12:10:57 +0700

    fix bug

--- app/Http/Transformers/ProductFileTransformer.php
+++ app/Http/Transformers/ProductFileTransformer.php
@@ -1,28 +1,28 @@
 <?php namespace App\Http\Transformers;
 
 use League\Fractal\TransformerAbstract;
-use App\Image\Imagy;
+//use App\Image\Imagy;
 use App\Models\ArtifactFile;
 
-class ArtifactFileTransformer extends TransformerAbstract
-{
-
-  public function transform(ArtifactFile $item)
-    {
-        $data = [
-            "id" => $item->id,
-            "artifactId" => $item->artifact_id,
-            "filename" => $item->filename,
-            "path" => $item->path,
-            "fileType" => $item->file_type,
-            "isMain" => $item->is_main,
-            "order" => $item->order,
-        ];
-        return $data;
-    }
-
-    private function getPath($path)
-    {
-        return asset($path);
-    }
-}
+//class ArtifactFileTransformer extends TransformerAbstract
+//{
+//
+//  public function transform(ArtifactFile $item)
+//    {
+//        $data = [
+//            "id" => $item->id,
+//            "artifactId" => $item->artifact_id,
+//            "filename" => $item->filename,
+//            "path" => $item->path,
+//            "fileType" => $item->file_type,
+//            "isMain" => $item->is_main,
+//            "order" => $item->order,
+//        ];
+//        return $data;
+//    }
+//
+//    private function getPath($path)
+//    {
+//        return asset($path);
+//    }
+//}

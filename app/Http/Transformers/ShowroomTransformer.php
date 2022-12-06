5b4fee9114b26ac62fe5b148004f5045808115c0
Anhtd <anhtd@beetsoft.com.vn>
2022-12-07 03:19:07 +0700

    fix api

--- app/Http/Transformers/ShowroomTransformer.php
+++ app/Http/Transformers/ShowroomTransformer.php
@@ -1,5 +1,6 @@
 <?php namespace App\Http\Transformers;
 
+use App\Models\Artifact;
 use App\Models\Showroom;
 use App\Services\StorageService;
 use League\Fractal\TransformerAbstract;
@@ -15,22 +16,31 @@ class ShowroomTransformer extends TransformerAbstract
   public function transform(Showroom $item)
   {
       $storageService = new StorageService();
-      return [
-            "id" => (int)$item->id,
-            "name" => $item->name,
-            "description" => $item->description,
-            "code" => $item->code,
-            "status" => $item->status,
-            "createdAt" => (string)$item->created_at,
-            "updatedAt" => (string)$item->updated_at,
-            'chat' => $item->chat,
-            'image_url_1' => $item->image_url_1 ? $storageService->url($item->image_url_1) : "",
-            'image_url_2' => $item->image_url_2 ? $storageService->url($item->image_url_2) : "",
-            'image_url_3' => $item->image_url_3 ? $storageService->url($item->image_url_3) : "",
-            'video_url'  => $item->video_url ? $storageService->url($item->video_url) : "",
-            'logo_url' => $item->logo_url ? $storageService->url($item->logo_url) : "",
-            'shopee_url' => $item->shopee_url,
-            '3d_link' => $item->d_link,
-      ];
+      $artifacts = Artifact::where('showroom_id', (int)$item->id)->orderby('sort')->get();
+      $dataItem = [];
+      foreach ($artifacts as $key => $artifact){
+          $dataItem['item_'.$key+1 ] =  [
+            'image_url' =>  $artifact->image_url ? $storageService->url($artifact->image_url) : "",
+            'title_url' =>  $artifact->title,
+            'description_url' =>  $artifact->description,
+          ];
+      }
+      return array_merge($dataItem, [
+          "id" => (int)$item->id,
+          "name" => $item->name,
+          "description" => $item->description,
+          "code" => $item->code,
+          "status" => $item->status,
+          "createdAt" => (string)$item->created_at,
+          "updatedAt" => (string)$item->updated_at,
+          'live_chat_url' => $item->live_chat_url,
+          'company_profile_url' => $item->company_profile_url,
+          'web_url' => $item->web_url,
+          'video_url_1'  => $item->video_url_1 ? $storageService->url($item->video_url_1) : "",
+          'video_url_2'  => $item->video_url_2 ? $storageService->url($item->video_url_2) : "",
+          'logo_url_1' => $item->logo_url_1 ? $storageService->url($item->logo_url_1) : "",
+          'shopee_url' => $item->shopee_url,
+          '3d_link' => $item->d_link,
+      ]);
   }
 }
